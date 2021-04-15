298906ff4fdf92e3bcb8c13dacc783e6a38e0fcb
kaieaa <74535489+kaieaa@users.noreply.github.com>
2021-04-16 00:49:25 +0300

    Update students.component.ts

--- front-end/src/app/students/students.component.ts
+++ front-end/src/app/students/students.component.ts
@@ -1,65 +1,62 @@
-/*import { Component } from '@angular/core';
-import { LoginStore } from '../../stores/LoginStore';
-import { RootStore } from '../../stores/RootStore';
-import API from '../../util/ApiUtil';
-
-
-@Component({
-  selector: 'app-content',
-  providers: [LoginStore],
-  templateUrl: './students.component.html'
-})
-export class StudentsComponent {
-
-  public values: any;
-
-  public fetchOnClick = async () => {
-    try {
-      const response = await API.get('/students');
-      this.values = response.data;
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-}*/
-
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RootStore } from '../../stores/RootStore';
+import { LoginStore } from '../../stores/LoginStore';
 import { StudentsStore } from '../../stores/StudentsStore';
+
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
 
 @Component({
   selector: 'app-content',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent implements OnInit {
+  public students: Student[] = [];
+  public user: any;
 
   studentsForm: FormGroup;
   loading = false;
   submitted = false;
   error = false;
-  private studentsStore: StudentsStore;
+  public studentsStore: StudentsStore;
+  public loginStore: LoginStore;
 
-  constructor(rootStore: RootStore, private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router) {
+  constructor(
+    rootStore: RootStore,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.studentsStore = rootStore.studentsStore;
+    this.loginStore = rootStore.loginStore;
+    this.user = this.loginStore.user?.id;
+    console.log(this.user);
     this.studentsForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', Validators.required],
-      phone: ['', Validators.required]
+      phone: ['', Validators.required],
     });
   }
 
   public data: any;
 
-  async ngOnInit(): Promise<void> {
+  async ngOnInit(): Promise<void> {}
 
-  }
-
-  get f(): {[key: string]: AbstractControl} {
+  get f(): { [key: string]: AbstractControl } {
     return this.studentsForm.controls;
   }
 
@@ -73,7 +70,13 @@ export class StudentsComponent implements OnInit {
     this.error = false;
     this.loading = true;
 
-    this.studentsStore.students(this.f.firstName.value, this.f.lastName.value, this.f.email.value, this.f.phone.value)
+    this.studentsStore
+      .addStudent(
+        this.f.firstName.value,
+        this.f.lastName.value,
+        this.f.email.value,
+        this.f.phone.value
+      )
       .then((success) => {
         if (success) {
           this.router.navigateByUrl('/students');
