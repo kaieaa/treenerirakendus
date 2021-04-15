af1ec246a28d587ea040b846fc4f9c18f5ac10fb
kaieaa <74535489+kaieaa@users.noreply.github.com>
2021-04-16 00:44:30 +0300

    Update StudentsStore.ts

--- front-end/src/stores/StudentsStore.ts
+++ front-end/src/stores/StudentsStore.ts
@@ -1,11 +1,29 @@
 import API from '../util/ApiUtil';
+import { Student } from '../app/students/students.component';
+import { LoginStore } from '../stores/LoginStore';
+import { RootStore } from './RootStore';
 
 export class StudentsStore {
   public status = 'FETCHING';
   public loginData: any = {};
-  public user: any;
+  public userId: any;
 
-  public students = async (
+  public students: Student[] = [];
+
+  public constructor() {
+    console.log(this.userId);
+    this.fetchAllStudents(this.userId);
+  }
+  public fetchAllStudents = async (userId: number) => {
+    try {
+      const response = await API.get('/students', { params: { userId } });
+      this.students = response.data.students;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  public addStudent = async (
     firstName: string,
     lastName: string,
     email: string,
@@ -13,12 +31,12 @@ export class StudentsStore {
   ) => {
     try {
       const response = await API.get('/students');
-      this.user = response.data.user;
+      this.students = response.data.students;
       this.status = 'FETCHED';
       return true;
     } catch (e) {
       console.error(e);
-      this.user = null;
+      //this.students = null;
       this.status = 'ERROR';
       return false;
     }
