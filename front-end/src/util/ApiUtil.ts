43614b611b27e096deee9e062591a30e3d00b72a
kaieaa <44272701+kaieaa@users.noreply.github.com>
2021-04-19 23:23:37 +0300

    api baseURL changed

--- front-end/src/util/ApiUtil.ts
+++ front-end/src/util/ApiUtil.ts
@@ -3,7 +3,7 @@ import { stringify } from 'qs';
 
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api', // API baasurl
+  baseURL: 'http://http://135.181.100.118:3000/api', // API baasurl
   timeout: 10000,
   paramsSerializer: (params) => stringify(params, { arrayFormat: 'repeat' }),
   withCredentials: true // true on vaja kui eri domeenil et cookied kaasa läheks. Vb vaja backendi poolel lubada ka.
